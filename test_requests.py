4b0b06263e0e33656477ae92b70f9a22834f9578
Ilya Myakishev <ilyamyakishev96@gmail.com>
2023-11-14 19:27:44 +0300

    unused import deleted

--- test_requests.py
+++ test_requests.py
@@ -2,7 +2,7 @@ import requests
 
 URL = "http://127.0.0.1:8000/get_form"
 
-def post_request(user_unput1, user_input2):
+def post_request(user_input1, user_input2):
     data = {
         "f_name1": f"{user_input1}",
         "f_name2": f"{user_input2}"
